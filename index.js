85897e44e5081790b057ecfb99653a7569d389bb
weihaoke13 <weihaoke13@gmail.com>
2020-08-05 15:56:40 -0700

    ad + new + footer

--- index.js
+++ index.js
@@ -36,6 +36,39 @@ $(document).ready(function(){
         $grid.isotope({ filter: filterValue});
     })
 
+    // new phones owl carousel
+    $("#new-phones .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000 : {
+                items: 5
+            }
+        }
+    });
+
+    // blogs owl carousel
+    $("#blogs .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            }
+        }
+    })
+
 
 
 })
